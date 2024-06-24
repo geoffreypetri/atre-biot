7e161223291e4dbb42019ad0c1022a82b11dfe50
geoffreypetri <g.petri@me.com>
2024-06-24 16:47:10 +0200

    first page

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>taichi-biot</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/normalize.css" />
       </head>
       <body>
         <Component />
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,8 +4,42 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="">
-
-    </div>
+    <>
+      <main>
+          <h2>Une pratique martiale pour une bonne santé</h2>
+          <p>Le <i>Tai Chi Chuan</i> art martial chinois est aujourd'hui pratiqué comme une gymnastique traditionnelle accessible à tous.
+          Sa pratique régulière apporte santé, calme, équilibre, contrôle du stress et relaxation.</p>
+      </main>
+      <yinyang>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="-40 -40 80 80">
+          <circle r="39"/>
+          <path fill="#fff" d="M0,38a38,38 0 0 1 0,-76a19,19 0 0 1 0,38a19,19 0 0 0 0,38"/>
+          <circle r="5" cy="19" fill="#fff"/>
+          <circle r="5" cy="-19"/>
+      </svg>
+      </yinyang>
+      <map>
+        <a href="https://www.google.com/maps/place/Complexe+Sportif+Pierre+Operto/@43.6277299,7.097305,17z/data=!3m1!4b1!4m6!3m5!1s0x12cdd4b62399d627:0xcb72465e6540bb8a!8m2!3d43.6277299!4d7.0998799!16s%2Fg%2F11c44dkpdc?entry=ttu">
+        <span>Complexe Sportif Pierre Operto<br/>
+        Salle Paul Gilardi<br/>
+        Chemin des Combes 06410 Biot</span></a>
+      </map>
+      <infos>
+        <ul>
+          <li>Mardi 19h &ndash; 20h</li>
+          <li>Tarif à l'année : 220 €</li>
+        </ul>
+      </infos>
+      <contact>
+        <ul>
+          <li>Jean-Marc Guibilato</li>
+          <li>06 14 60 81 34</li>
+          <li>academietaichi@orange.fr</li>
+        </ul>
+      </contact>
+      <biot>
+        <img src="/2022_Logo_320_50_WP-1.png" />
+      </biot>
+    </>
   );
 }
